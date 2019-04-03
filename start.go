a97a54a7b0025f8021f7b75a98957390fc3fe6b1
Thyrael <robin.baraffe@gmail.com>
2019-04-03 13:17:21 +0200

    Init repo & create golang template

---
+++ start.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+var  playerNamePointCounter = 0
+var  computerPointCounter = 0
+
+
+func main() {
+    fmt.Println("Welcome to Rock Paper Scissors in Golang World")
+    fmt.Println("Enter your name : ")
+
+    buf := bufio.NewReader(os.Stdin)
+    fmt.Print(">")
+    playerName, err := buf.ReadBytes('\n')
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println("Ok "+ string(playerName) +"Let's play")
+
+    for sumGamePoint(playerNamePointCounter, computerPointCounter) < 10 {
+        play()
+    }
+
+    if playerNamePointCounter > computerPointCounter {
+        fmt.Println("Ok player win")
+    } else if computerPointCounter > playerNamePointCounter {
+        fmt.Println("Ok computer win")
+    } else {
+        fmt.Println("Problem detected")
+    }
+    fmt.Println("End of game")
+}
+
+func sumGamePoint(playerTotal int, computerTotal int) (int) {
+    total := playerTotal + computerTotal
+    fmt.Println("Total :", total)
+    return total
+}
+
+func play() {
+    fmt.Println("Play")
+    playerNamePointCounter += 1
+    playerPlay()
+}
+
+func playerPlay() {
+    fmt.Println("Your turn : ")
+    buf := bufio.NewReader(os.Stdin)
+    fmt.Print(">")
+    playerInput, err := buf.ReadBytes('\n')
+        if err != nil {
+            fmt.Println(err)
+        }
+}
+
+func computerPlay() {
+
+}
+
+
